fce6d27e0b4d1720b64214bf54d9c91a250d7751
gbasilio321 <gubasilio2014@outlook.com>
2025-08-19 12:48:29 -0300

    belonging atualizado

---
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,65 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private long id;
+   private String name;
+
+    public GameList() {
+    }
+
+    public GameList(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GameList other = (GameList) obj;
+        return this.id == other.id;
+    }
+
+    
+}
